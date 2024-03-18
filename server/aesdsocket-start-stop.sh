4622f9fdf6513eab8055f5b336a3e2756fc80efc
SwathiVenkatachalam <swve3352@colorado.com>
2024-03-18 14:45:04 -0600

    Adding dedicated .sh for aesdchardriver

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -5,14 +5,10 @@
 case "$1" in
     start)
         echo "Starting aesdsocket server"
-        #Adding A8
-        /usr/bin/aesdchar_load
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket server"
-        #Adding A8
-        /usr/bin/aesdchar_unload
         start-stop-daemon -K -n aesdsocket
         ;;
     *)
